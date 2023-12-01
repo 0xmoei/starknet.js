3b76b0dd95b4b8cee6e704063501c66920866ad1
Moei <90371338+0xmoei@users.noreply.github.com>
2023-12-01 01:44:58 +0100

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ export { IS_BROWSER } from './utils/encode';
 export const TEXT_TO_FELT_MAX_LEN = 31;
 
 export const HEX_STR_TRANSACTION_VERSION_2 = '0x1';
-export const HEX_STR_TRANSACTION_VERSION_2 = '0x2';
+export const HEX_STR_TRANSACTION_VERSION_3 = '0x2';
 export const BN_TRANSACTION_VERSION_1 = 1n;
 export const BN_TRANSACTION_VERSION_2 = 2n;
 export const BN_FEE_TRANSACTION_VERSION_1 = 2n ** 128n + BN_TRANSACTION_VERSION_1;
